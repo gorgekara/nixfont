d7ff3312c6f16436fbf9096baacd6e2b5279fe1d
Gjorge Karakabakov <gorgekara@gmail.com>
2016-09-07 16:14:34 +0200

    Link to google web fonts single font page

--- app/feapp/app.constants.js
+++ app/feapp/app.constants.js
@@ -1,4 +1,5 @@
 NixFont.constant('CONSTANTS', {
+  'GOOGLE_WEB': 'https://fonts.google.com/specimen/',
   'API_KEY': 'AIzaSyCvq2JszVv4-Dd0gR9afwhM8gdtRFUouJ0',
   'API_URL': 'https://www.googleapis.com/webfonts/v1/webfonts',
 });
